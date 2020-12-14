d8f8aa474f788608d27d33a2e12f9ae20e569b44
Sparsh Jain <sparshjain265@gmail.com>
2020-12-14 22:40:35 +0530

    android - API

--- notes/codes/android/Pokedex/app/src/main/java/com/example/pokedex/PokedexAdapter.java
+++ notes/codes/android/Pokedex/app/src/main/java/com/example/pokedex/PokedexAdapter.java
@@ -1,6 +1,8 @@
 package com.example.pokedex;
 
+import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,40 +13,61 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class PokedexAdapter extends RecyclerView.Adapter<PokedexAdapter.PokedexViewHolder> {
-    public static class PokedexViewHolder extends RecyclerView.ViewHolder {
-        public LinearLayout containerView;
-        public TextView textView;
+    private final List<Pokemon> pokemon = new ArrayList<>();
+    private RequestQueue requestQueue;
 
-        PokedexViewHolder(View view) {
-            super(view);
-            containerView = view.findViewById(R.id.pokedex_row);
-            textView = view.findViewById(R.id.pokedex_row_text_view);
-
-            containerView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Pokemon current = (Pokemon) containerView.getTag();
-                    Intent intent = new Intent(v.getContext(), PokemonActivity.class);
-                    intent.putExtra("name", current.getName());
-                    intent.putExtra("number", current.getNumber());
-
-                    v.getContext().startActivity(intent);
-                }
-            });
-        }
+    PokedexAdapter(Context context){
+        requestQueue = Volley.newRequestQueue(context);
+        loadPokemon();
     }
 
-    private List<Pokemon> pokemon = Arrays.asList(
-            new Pokemon("Bulbasaur", 1),
-            new Pokemon("Ivysaur", 2),
-            new Pokemon("Venusaur", 3)
-    );
+    public void loadPokemon() {
+        String url = "https://pokeapi.co/api/v2/pokemon?limit=151";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray results = response.getJSONArray("results");
+                    for (int i = 0; i < results.length(); i++) {
+                        JSONObject result = results.getJSONObject(i);
+                        String name = result.getString("name");
+                        pokemon.add(new Pokemon(
+                                name.substring(0, 1).toUpperCase() + name.substring(1),
+                                result.getString("url")
+                        ));
+                    }
+
+                    notifyDataSetChanged();
+                } catch (JSONException e) {
+                    Log.e("cs50", "Json error", e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("cs50", "Pokemon list error");
+            }
+        });
+
+        requestQueue.add(request);
+    }
 
     @NonNull
     @Override
@@ -65,5 +88,26 @@ public class PokedexAdapter extends RecyclerView.Adapter<PokedexAdapter.PokedexV
     @Override
     public int getItemCount() {
         return pokemon.size();
+    }
+
+    public static class PokedexViewHolder extends RecyclerView.ViewHolder {
+        public LinearLayout containerView;
+        public TextView textView;
+
+        PokedexViewHolder(View view) {
+            super(view);
+            containerView = view.findViewById(R.id.pokedex_row);
+            textView = view.findViewById(R.id.pokedex_row_text_view);
+
+            containerView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Pokemon current = (Pokemon) containerView.getTag();
+                    Intent intent = new Intent(v.getContext(), PokemonActivity.class);
+                    intent.putExtra("url", current.getUrl());
+                    v.getContext().startActivity(intent);
+                }
+            });
+        }
     }
 }
